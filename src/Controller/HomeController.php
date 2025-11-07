87241421555548247ad4038c97d6e823cfe2b770
Bien <lopao0012@gmail.com>
2025-11-07 08:47:05 +0800

    Rename DashboardController to HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        return new Response("Welcome to the dashboard! (Protected Area)");
+    }
+}
